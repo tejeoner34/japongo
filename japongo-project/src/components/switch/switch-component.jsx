a7c476f111e0f874b520d8a0f611ff114b9b7a99
tejeoner34 <atzarco37@gmail.com>
2021-12-21 18:48:02 +0100

    when mentioned the @name appears in bold

--- japongo-project/src/components/switch/switch-component.jsx
+++ japongo-project/src/components/switch/switch-component.jsx
@@ -28,7 +28,7 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
     },
   },
   '& .MuiSwitch-thumb': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#555454',
     width: 32,
     height: 32,
     '&:before': {
