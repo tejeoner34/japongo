de4c2b4377d7f4c992deb8dc7c049c263954fef3
tejeoner34 <atzarco37@gmail.com>
2023-03-04 13:54:38 +0100

    feat: updated server url

--- japongo-project/src/global/global-variable.js
+++ japongo-project/src/global/global-variable.js
@@ -1,3 +1,3 @@
 
-export const serverUrl = 'https://japongo-project.herokuapp.com/img'
-export const serverFetch = 'https://japongo-project.herokuapp.com/'
+export const serverUrl = 'https://japongo-project.onrender.com/img'
+export const serverFetch = 'https://japongo-project.onrender.com/'
